5828d7c70c1bc395fb0fc7406aa38da4d8834eed
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-03 02:58:46 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,12 @@ class ListsController < ApplicationController
 
   # 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -26,13 +25,13 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
